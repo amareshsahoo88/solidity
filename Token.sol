b92d48cf5e0d5bb65ad8dc8dde043b7f24de459c
amareshsahoo88 <112602756+amareshsahoo88@users.noreply.github.com>
2022-10-21 14:24:09 +0530

    Add files via upload

--- Token.sol
+++ Token.sol
@@ -6,26 +6,27 @@ import "hardhat/console.sol";
 
 contract Token{
 
+    // Declaring the state variables
     string public name="HardHat Token";
     string public symbol ="HHT";
     uint public totalSupply = 10000;
-
     address public owner;
 
-    mapping(address=>uint) balances;
+    mapping(address=>uint) balances;  // mapping the address to tokens
 
+    // Initialising the contract owner balance with total supply
     constructor(){
         balances[msg.sender] = totalSupply;
         owner=msg.sender;
     }
-
+    // Setting up the transfer function from contract owner to individual addresses
     function transfer(address to, uint amount) external {
 
         require(balances[msg.sender]>=amount , "not enough balance");
         balances[msg.sender]-=amount;
         balances[to] += amount;
     }
-
+    // setting up the check balance function for the addresses
     function balanceof(address account) external view returns(uint){
         return balances[account];
     }
