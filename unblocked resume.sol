cc8f3f321f90b6985f1747c1992b18d335d2952e
amaresh sahoo <112602756+amareshsahoo88@users.noreply.github.com>
2022-11-05 00:38:14 +0530

    Update unblocked resume.sol

--- unblocked resume.sol
+++ unblocked resume.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.13;
 contract unblockedResume {
 
     struct student {
-
+        
         string name;
         string addr ;
         uint phNo ;
         string email;
-        
+        bool conf;
         }
 
     struct education_school {
@@ -19,6 +19,7 @@ contract unblockedResume {
         uint entry_yr;
         uint exit_yr;
         address schoolId;
+        bool conf;
     }
 
     struct education_coll {
@@ -28,6 +29,7 @@ contract unblockedResume {
         uint entry_yr;
         uint exit_yr;
         address collId;
+        bool conf;
     }
 
     struct professional_exp{
@@ -37,6 +39,7 @@ contract unblockedResume {
         uint join_yr;
         uint exit_yr;
         address compId;
+        bool conf;
 
     }
     
@@ -65,7 +68,6 @@ contract unblockedResume {
         eduschool[_add].entry_yr = _entry_yr;
         eduschool[_add].exit_yr = _exit_yr;
         eduschool[_add].schoolId = _schoolId;
-
     }
 
     function setcoll(address _addr , string memory _coll , uint _coll_percent , uint _entry_yr , uint _exit_yr , address _collId) public {
@@ -76,7 +78,43 @@ contract unblockedResume {
         coll[_addr].collId = _collId;
     }
 
+    function setProfExp(address _addr , string memory _comp_name , string memory _position , uint _join_yr , uint _exit_yr , address _compId) public {
+        comp[_addr].comp_name = _comp_name ;
+        comp[_addr].position = _position ;
+        comp[_addr].join_yr = _join_yr ;
+        comp[_addr].exit_yr = _exit_yr ;
+        comp[_addr].compId = _compId ;
+    }
 
+
+    function confirmSchooldata(address _stuScl) public {
+
+        require(msg.sender == eduschool[_stuScl].schoolId , "You are not authorised");
+        eduschool[_stuScl].conf = true;
+    }
+
+    function confirmColldata(address _stucoll) public {
+
+        require(msg.sender == coll[_stucoll].collId , "You are not authorised");
+        coll[_stucoll].conf = true;
+    }
+
+     function confirmProff(address _compId) public {
+
+        require(msg.sender == comp[_compId].compId , "You are not authorised");
+        comp[_compId].conf = true;
+    }
+
+    function jobEligibility(address _addr) public returns(string memory){
+
+        if ((eduschool[_addr].conf = true) && (coll[_addr].conf = true) && (comp[_addr].conf = true))
+        {
+            return "You are eligible for a Job.";
+        }
+        else {
+            return "You are not eligiblefor a job.";
+        }
+    }
 
 
 }
